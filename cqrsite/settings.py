9368f58466728a459c20e66432cd01cdc6bebb7f
kannie <kangokseo@gmail.com>
2024-05-21 15:34:08 +0900

    h0521

--- cqrsite/settings.py
+++ cqrsite/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-#SECRET_KEY="django-insecure-h9=sp9%*hddrh+nl+s*du))0er3c^hsiypm8)j2sr^+&xlz)j5"
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY="django-insecure-h9=sp9%*hddrh+nl+s*du))0er3c^hsiypm8)j2sr^+&xlz)j5"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
